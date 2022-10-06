0eee08fde7d9a994965a7c01b12d9e18ba5d6f99
kml htb <kmlhtbdev@gmail.com>
2022-10-06 20:22:05 +0300

    mod düzeltme

--- src/ciftSayi.java
+++ src/ciftSayi.java
@@ -8,7 +8,7 @@ public class ciftSayi {
         k =input.nextInt();
 
         for (int i=1;i <= k;i++) {
-            if ((i % 3 == 0) || (i % 3 == 0)) {
+            if ((i % 3 == 0) || (i % 4 == 0)) {
                 System.out.println(i);
                 total += i;
                 loop += 1;
